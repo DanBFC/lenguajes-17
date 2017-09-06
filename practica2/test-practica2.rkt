4843ae17895f5e2c7e96d5b29884e3228e17fe61
Daniel Beltrán <d.beltran@gendra.mx>
2017-09-06 01:16:51 -0500

    Add practica2 and add the exercise 2.1.

---
+++ practica2/test-practica2.rkt
@@ -0,0 +1,34 @@
+#lang plai
+
+(require "practica2.rkt")
+
+(print-only-errors)
+
+#| Módulo para pruebas unitarias de la práctica 2 |#
+
+;; Pruebas para funcion->string
+
+#| ... Aquí van las pruebas (Borrar este comentario) ... |#
+
+;; Pruebas para evalua
+
+#| ... Aquí van las pruebas (Borrar este comentario) ... |#
+
+;; Pruebas para deriva
+
+#| ... Aquí van las pruebas (Borrar este comentario) ... |#
+
+;; Pruebas para verifica
+
+#| ... Aquí van las pruebas (Borrar este comentario) ... |#
+
+;; Pruebas para acepta?
+
+#| ... Aquí van las pruebas (Borrar este comentario) ... |#
+
+;; Pruebas para calc-a
+
+#| ... Aquí van las pruebas (Borrar este comentario) ... |#
+
+;; Pruebas para calc-c
+#| ... Aquí van las pruebas (Borrar este comentario) ... |#
